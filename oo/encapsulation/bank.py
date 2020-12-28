b4d90841b0ca619d33424daab24c837a5cf7948a
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-12-28 19:29:43 -0300

    Added heritage and overwrite

--- oo/encapsulation/bank.py
+++ oo/encapsulation/bank.py
@@ -1,4 +1,4 @@
-class Account:
+class Bank1Account(object):
 
     # Cannot access attributes directly
     # Private attributes: add __ before attribute name
@@ -11,6 +11,23 @@ class Account:
 
     def withdraw(self, value):
         self.__total -= value
+        self.__total -= 1
 
     def get_total(self):
         return self.__total
+
+
+# Heritage
+class Bank2Account(Bank1Account):
+    # Used to use all inherited attributes/methods
+    # pass
+
+    # Overwrite constructor method
+    def __init__(self, number, cvv):
+        super(Bank2Account, self).__init__(number)
+        self.cvv = cvv
+
+    # Method overwrite
+    def withdraw(self, value):
+        self._Bank1Account__total -= value
+        self._Bank1Account__total -= 2
