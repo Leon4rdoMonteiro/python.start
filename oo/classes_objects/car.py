7537b46245f885417e24804d6fbbf587d875f285
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2020-12-28 18:21:33 -0300

    Added classes and objects

---
+++ oo/classes_objects/car.py
@@ -0,0 +1,12 @@
+class Car:
+
+    # Class attributes
+    x = 'Abcd'
+
+    # Constructot method
+    def __init__(self):
+        # Object attributes
+        self.name = 'Gol'
+        self.maker = 'Wolks'
+        self.year = 2018
+        self.color = 'Silver'
